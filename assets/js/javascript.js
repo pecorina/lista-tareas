36faf55ae37a8f4194c516a0deb2016ac73c2760
pecorina <constanza.pecori@gmail.com>
2017-04-27 13:28:10 -0300

    creando caja de ingreso de datos

---
+++ assets/js/javascript.js
@@ -0,0 +1,83 @@
+var lista =
+
+[
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "quo adipisci enim quam ut ab",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "molestiae perspiciatis ipsa",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "illo est ratione doloremque quia maiores aut",
+    "completed": true
+  }
+
+function botonInfo(){
+  var info = document.getElementById('lista');
+  var enlacesId = document.getElementsByTagName('id')
+  
+  var contador = 0;
+  for (var i = 0; i < enlacesId.length[10]; i++){
+    if(enlacesId[i].href ){
+    contador++; //contador = contador + i
+  }
+}
+
+
+
+info.innerHTML = 'El total de tareas es: <span class="resultado">' + 
+enlaces.length + '</span> <br> El total de párrafos es: ' + parrafos.length + 
+'<br> Los enlaces que llevan a Laboratoria son: '+ contador;
+}
+
+
